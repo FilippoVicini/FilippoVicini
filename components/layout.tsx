8f95754cb3339f1d1b12278da99fa1e311f391aa
Filippo Vicini <filippo.vicini2@gmail.com>
2022-07-30 14:07:39 +0200

    fourth commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen justify-between font-content antialiased">
-      <Header />
+
       <main className="mb-auto">{children}</main>
     </div>
   )
